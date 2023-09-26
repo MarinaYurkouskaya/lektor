4f5c79770b57d171151a8996c3d6a38fd4126f84
marina <marinapetrochuk@gmail.com>
2023-09-27 00:24:44 +0300

    seminar1

---
+++ seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n * n);
+
+Console.Write ("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b*b==a)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
